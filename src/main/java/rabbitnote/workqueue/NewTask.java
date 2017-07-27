6f2d97ed97996d71e86060958865e8b902abf8ff
shalk <xiaokun@svmsoft.com.cn>
2017-07-27 15:53:31 +0800

    complete work queues

--- src/main/java/rabbitnote/workqueue/NewTask.java
+++ src/main/java/rabbitnote/workqueue/NewTask.java
@@ -1,5 +1,35 @@
 package rabbitnote.workqueue;
 
+import java.awt.datatransfer.StringSelection;
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.BasicProperties;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
 public class NewTask {
+
+    public final static String host = "10.0.33.145";
+    public final static String queue = "hello2";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(host);
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(queue, true, false, false, null);
+        for(int i = 0; i < 10 ; i++) {
+            Random rand= new Random();
+            String msg = "hehe " + i + " " + new String(new char[rand.nextInt(20)+1]).replace('\0', '.');
+            System.out.println("[x] send:" + msg);
+            channel.basicPublish("", queue, false, false, MessageProperties.PERSISTENT_TEXT_PLAIN, msg.getBytes());
+        }
+        channel.close();
+        connection.close();
+    }
 
 }
--- src/main/java/rabbitnote/workqueue/Worker.java
+++ src/main/java/rabbitnote/workqueue/Worker.java
@@ -1,5 +1,56 @@
 package rabbitnote.workqueue;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
 public class Worker {
 
+    public final static String host = "10.0.33.145";
+    public final static String queue = "hello2";
+
+    public static void doWork(String msg) {
+        // do something
+        for (char c : msg.toCharArray()) {
+            try {
+                if (c == '.')
+                    Thread.sleep(300);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(host);
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(queue, true, false, false, null);
+        
+        channel.basicQos(1);
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
+                    byte[] body) throws IOException {
+                String msg = new String(body, "UTF-8");
+                System.out.println("[x] Reciever" + msg);
+                try {
+                    doWork(msg);
+                } finally {
+                    channel.basicAck(envelope.getDeliveryTag(), false);
+                }
+            }
+        };
+        channel.basicConsume(queue, false, "", false, false, null, consumer);
+    }
 }
